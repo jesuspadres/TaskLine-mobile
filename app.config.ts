f5598830c31bb149d413adbc5702b55b59735f27
Jesus Padres <jesuspadres@gmail.com>
2026-02-18 18:52:31 -0700

    Notification fixes

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY ?? '';
 export default ({ config }: ConfigContext): ExpoConfig => ({
   name: 'TaskLine',
   slug: 'taskline-mobile',
-  version: '1.0.0',
+  version: '1.0.1',
   orientation: 'portrait',
   icon: './assets/icon.png',
   scheme: 'taskline',
